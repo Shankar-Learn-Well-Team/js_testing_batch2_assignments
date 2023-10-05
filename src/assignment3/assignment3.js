8b50faf56bb4101a43cf4efdbadd2b7dafe9ebe8
aman_bhatnagar <bhatnagaraman2808@gmail.com>
2023-10-05 19:17:44 +0530

    changeee

---
+++ src/assignment3/assignment3.js
@@ -0,0 +1,16 @@
+// program to check if the number is odd
+
+const number = 13;
+if (number>0){
+if(number % 2 == 0) {
+    console.log("True ....The number is even.");
+}
+
+// if the number is odd
+else {
+    console.log("True...The number is odd.");
+}
+}
+else{
+    console.log("number is out of Range")
+}
---
+++ src/assignment4/assign4.js
@@ -0,0 +1,13 @@
+// Creating a map using Map object
+let newMap = new Map();
+
+// Adding values to the map
+newMap.set("apple",'a fruit' );
+newMap.set("banana",'a tropical fruit');
+
+// Logging map object to console
+newMap.forEach((values, keys) => {
+	console.log(values, keys);
+});
+
+ 
