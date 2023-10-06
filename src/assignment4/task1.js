043b3444107f6251e11f9ea8efacbf45e0981fd3
Pawar, Akash <Akash.pawar@wolterskluwer.com>
2023-10-06 23:29:31 +0530

    Upadted Message

--- src/assignment4/task1.js
+++ src/assignment4/task1.js
@@ -1,4 +1,6 @@
 const car = { make: 'Toyota', model: 'Camry', year: 2022 };
 const { make, year } = car;
+
 const extractedProperties = { make, year };
-console.log(extractedProperties);
+
+console.log(extractedProperties); // Output: { make: 'Toyota', year: 2022 }
