6512f0b8671378ddddf57bda10efd0758c84c418
Pranjli Narkhede <pranjli.narkhede@wolterskluwer.com>
2023-10-06 17:55:55 +0530

    assignment 1 and 2 added

---
+++ src/assignment1/task1.js
@@ -0,0 +1,42 @@
+// Task 1: Create an array of numbers and write a function to calculate their sum.
+
+let array1 = [11, 12, 3, 14, 5];
+
+function addarray() {
+  let array_sum = 0;
+  for (let i = 0; i < array1.length; i++) {
+    array_sum = array_sum + array1[i];
+  }
+  return array_sum;
+}
+console.log(addarray());
+
+
+// Task 2: Write a function that finds the maximum value in an array of numbers.
+
+let array2 = [1, 2, 13, 14, 5];
+
+//find max value using math.max function
+
+function findmaxno() {
+  let maxvalue = Math.max(...array2);
+  return maxvalue;
+}
+
+console.log(findmaxno());
+
+
+//find max value using for loop
+
+function findmaxnoUsingForloop() {
+
+  let temp = array2[0];
+  for (let i = 0; i < array2.length; i++) {
+    if (temp < array2[i]) {
+      temp = array2[i];
+    }
+  }
+  return temp;
+}
+
+console.log(findmaxnoUsingForloop());
--- src/core_modules/file.js
+++ src/core_modules/file.js
@@ -1,4 +1,63 @@
-const path = require('path');
+const fs=require('fs')
+
+// fs.readFile('./src/core_modules/example.txt','utf-8',(err,data)=>{
+//     if(err) throw err;
+//     console.log(data);
+// })
+
+
+// fs.writeFile(
+//     './src/core_modules/example.txt',
+//     'Hello, Node.js!!',
+//     'utf8',
+//     (err) => {
+//         if (err) throw err;
+//         console.log('File written successfully.');
+//     },
+// );
+
+
+// fs.mkdir('./src/core_modules/test', {}, (err) => {    
+//      if (err) throw err;     
+//      console.log('Folder created...'); 
+//     });
+
+
+ 
+
+// fs.opendir('./src/core_modules/test', {}, (err,dir) => {
+//     if (err) throw err;
+//     console.log('Folder opened...');
+//     console.log(dir);
+// });
+
+
+
+// const directoryPath = "./src/core_modules/test"
+
+// const customOpenDir = async() => {
+// try {
+//      fs.writeFileSync(directoryPath+'/rexample.txt', "Hello World")
+//       const dir = fs.opendirSync(directoryPath);
+//       for await (const dirent of dir)
+//      console.log(dirent.name);
+//      } catch (err) {
+//           console.error(err);
+// }
+// }
+
+// customOpenDir()
+
+
+const path=require('path');
+
+const fullPath1 = path.join(__dirname, 'files', 'example.txt');
+console.log(fullPath1);
+
+
+const fullPath = path.join(__dirname,'/foo', 'bar','..', 'baz/asdf', 'quux', 'example.txt');
+console.log(fullPath);
+
 
 // const fullPath = path.join(__dirname, 'files', 'example.txt');
 console.warn('1', path.basename(__dirname));
