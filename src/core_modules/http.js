5506e4107c26dea0fd143471b3c05e1090ee4c02
shankar-learnwell <shankarlearnwell@gmail.com>
2023-10-06 09:05:42 +0530

    HTTP Changes For User Path

--- src/core_modules/http.js
+++ src/core_modules/http.js
@@ -3,68 +3,42 @@ const http = require('http');
 function startServer() {
     const server = http.createServer((req, res) => {
         try {
-            if (req.method === 'GET') {
-                // Handle GET request
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                const responseObject = { message: 'This is a GET request.' };
-                res.end(JSON.stringify(responseObject));
-            } else if (req.method === 'POST') {
-                // Handle POST request
-                let requestData = '';
-
-                req.on('data', (chunk) => {
-                    requestData += chunk;
-                });
-
-                req.on('end', () => {
-                    const requestObject = JSON.parse(requestData);
+            if (req.url === '/user') {
+                if (req.method === 'GET') {
+                    // Handle GET request for /user
                     res.statusCode = 200;
                     res.setHeader('Content-Type', 'application/json');
-                    const responseObject = { receivedData: requestObject };
+                    const responseObject = {
+                        message: 'This is a GET request for /user.',
+                    };
                     res.end(JSON.stringify(responseObject));
-                });
-            } else if (req.method === 'PUT') {
-                // Handle PUT request
-                let requestData = '';
+                } else if (req.method === 'POST') {
+                    // Handle POST request for /user
+                    let requestData = '';
 
-                req.on('data', (chunk) => {
-                    requestData += chunk;
-                });
+                    req.on('data', (chunk) => {
+                        requestData += chunk;
+                    });
 
-                req.on('end', () => {
-                    const requestObject = JSON.parse(requestData);
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'application/json');
-                    const responseObject = { updatedData: requestObject };
-                    res.end(JSON.stringify(responseObject));
-                });
-            } else if (req.method === 'PATCH') {
-                // Handle PATCH request
-                let requestData = '';
-
-                req.on('data', (chunk) => {
-                    requestData += chunk;
-                });
-
-                req.on('end', () => {
-                    const requestObject = JSON.parse(requestData);
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'application/json');
-                    const responseObject = { patchedData: requestObject };
-                    res.end(JSON.stringify(responseObject));
-                });
-            } else if (req.method === 'DELETE') {
-                // Handle DELETE request
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                const responseObject = { message: 'This is a DELETE request.' };
-                res.end(JSON.stringify(responseObject));
+                    req.on('end', () => {
+                        const requestObject = JSON.parse(requestData);
+                        res.statusCode = 200;
+                        res.setHeader('Content-Type', 'application/json');
+                        const responseObject = { receivedData: requestObject };
+                        res.end(JSON.stringify(responseObject));
+                    });
+                } else {
+                    // Handle other HTTP methods for /user
+                    res.statusCode = 405;
+                    res.setHeader('Allow', 'GET, POST');
+                    res.end('Method Not Allowed');
+                }
             } else {
-                // Handle other HTTP methods
-                res.statusCode = 405;
-                res.setHeader('Allow', 'GET, POST, PUT, PATCH, DELETE');
-                res.end('Method Not Allowed');
+                // Handle other URL paths
+                res.statusCode = 404;
+                res.setHeader('Content-Type', 'application/json');
+                const responseObject = { message: 'Not Found' };
+                res.end(JSON.stringify(responseObject));
             }
         } catch (error) {
             console.error('Server error:', error);
