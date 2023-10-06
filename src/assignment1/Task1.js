b3b06687039f035650167cff9f4872c2f4b80370
RohitKambale <rohitkambale009@gmail.com>
2023-10-06 13:27:44 +0530

    Updated changes Task 1

---
+++ src/assignment1/Task1.js
@@ -0,0 +1,44 @@
+// Assignment - 1    
+    // Que- 1 Write a function declaration that calculates the "Square Of a Number"
+
+
+    //  1st 
+
+
+     function numbSquare()
+     {
+       
+        const number = 10;
+        return number*number;   
+        
+        
+      }
+      
+       const square = numbSquare();
+       console.log(square);
+     
+    //  2 nd  
+
+       function Squarenumb(number)
+    {
+
+        return number*number;
+
+        
+       }
+         let number = 10;
+         const result = Squarenumb(10);
+         console.log(result);
+         console.log(Squarenumb(100));
+
+
+    //  Que 2 - Write a function expression that converts a string to uppercase.
+
+    function toUprCase(){
+      
+        let string = "rohit";
+        let str = string.toUpperCase();
+            console.log(str);
+    }
+
+    toUprCase();
