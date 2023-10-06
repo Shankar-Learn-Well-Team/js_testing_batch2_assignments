493084daed48025a3e65b055251e9bab768acd75
Snehal <snehalkamble0312@gmail.com>
2023-10-06 13:22:54 +0530

    Updates changes in Task1

---
+++ src/assignment1/Task1.js
@@ -0,0 +1,15 @@
+
+function NumCheck (number)  {
+    if (number < 0) {
+
+        console.log ("It's a Negative Number");
+    } 
+ else if (number > 0){
+    console.log("It's a Positive Number");
+   }
+else {
+    console.log("Number is Zero");
+  }
+ }
+
+ NumCheck(-5); 
