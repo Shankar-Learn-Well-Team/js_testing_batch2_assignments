1d5c11e5415e4ecd3be5779e7910c3dfd1d1cbf0
Ashlesh <ashleshs9@gmail.com>
2023-10-07 21:01:49 +0530

    assignments 1 and 2

---
+++ src/assignment1/task_1.js
@@ -0,0 +1,25 @@
+// Task 1: Explain the difference between strings and numbers in JavaScript. Provide examples for each.
+
+// Numbers: Numbers can be both integers and floating point.
+// Strings: All the text information in JS can be considered to be a string
+
+// Example: 1. Numbers
+
+let floatingNum = 0.5;
+let intNum = 2;
+let floatingNegativeNum = -15.45;
+let yearNum = 2023;
+
+console.log("intNum: ", intNum);
+console.log("floatingNegativeNum: ", floatingNegativeNum);
+console.log("floatingNum: ", floatingNum);
+
+// Example: 2. String
+
+let string1 = 'Hello World';
+let nameString = "John Doe";
+let string2 = `some string`;
+
+console.log("string1: ", string1);
+console.log("nameString: ", nameString);
+console.log("string2: ", string2);
---
+++ src/assignment2/task_2.js
@@ -0,0 +1,7 @@
+// Task 2: Write a program that checks if a given variable is a boolean. Use the `typeof` operator."
+
+let var1 = true;
+let var2 = false;
+
+console.log("Type of Variable 1: ", typeof (var1));
+console.log("Type of Variable 2: ", typeof (var2));
